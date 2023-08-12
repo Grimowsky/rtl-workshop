f76724e5fdd7ca692b8e474be9bb6997eebdd47b
Grimm <rafalmirkowski@gmail.com>
2023-08-12 20:05:31 +0200

    add tailwind css setup and components

--- src/components/NavigateTest/NavigateTest.tsx
+++ src/components/NavigateTest/NavigateTest.tsx
@@ -1,11 +1,16 @@
 import { useNavigate } from 'react-router-dom';
+import { Button } from '../Button/Button';
 
 export const NavigateTest = () => {
   const navigate = useNavigate();
 
   return (
-    <button name="navigateBtn" onClick={() => navigate('/elsewhere')}>
-      navigate me back
-    </button>
+    <Button
+      variant="default"
+      size="default"
+      onClick={() => navigate('/elsewhere')}
+    >
+      Navigate ME
+    </Button>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
-import './index.css';
 import { App } from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
